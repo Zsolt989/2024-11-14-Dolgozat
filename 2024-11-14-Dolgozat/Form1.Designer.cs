3348228458eeeb679f49901349f90a1293bc297b
nagy.zsolt <nagy.zsolt@C19-04>
2024-11-14 20:45:02 +0100

    jóvanezígy

--- 2024-11-14-Dolgozat/Form1.Designer.cs
+++ 2024-11-14-Dolgozat/Form1.Designer.cs
@@ -29,13 +29,204 @@ namespace _2024_11_14_Dolgozat
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.trunk = new System.Windows.Forms.PictureBox();
+            this.foliage = new System.Windows.Forms.PictureBox();
+            this.head = new System.Windows.Forms.PictureBox();
+            this.body = new System.Windows.Forms.PictureBox();
+            this.arm = new System.Windows.Forms.PictureBox();
+            this.basket = new System.Windows.Forms.PictureBox();
+            this.handle = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trunk)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.foliage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.head)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.body)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basket)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.handle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trunk
+            // 
+            this.trunk.BackColor = System.Drawing.Color.Brown;
+            this.trunk.Location = new System.Drawing.Point(149, 166);
+            this.trunk.Name = "trunk";
+            this.trunk.Size = new System.Drawing.Size(41, 155);
+            this.trunk.TabIndex = 0;
+            this.trunk.TabStop = false;
+            // 
+            // foliage
+            // 
+            this.foliage.BackColor = System.Drawing.Color.Green;
+            this.foliage.Location = new System.Drawing.Point(92, 52);
+            this.foliage.Name = "foliage";
+            this.foliage.Size = new System.Drawing.Size(160, 123);
+            this.foliage.TabIndex = 1;
+            this.foliage.TabStop = false;
+            // 
+            // head
+            // 
+            this.head.BackColor = System.Drawing.Color.Beige;
+            this.head.Location = new System.Drawing.Point(305, 166);
+            this.head.Name = "head";
+            this.head.Size = new System.Drawing.Size(41, 67);
+            this.head.TabIndex = 0;
+            this.head.TabStop = false;
+            // 
+            // body
+            // 
+            this.body.BackColor = System.Drawing.Color.Blue;
+            this.body.Location = new System.Drawing.Point(316, 232);
+            this.body.Name = "body";
+            this.body.Size = new System.Drawing.Size(21, 89);
+            this.body.TabIndex = 0;
+            this.body.TabStop = false;
+            // 
+            // arm
+            // 
+            this.arm.BackColor = System.Drawing.Color.Beige;
+            this.arm.Location = new System.Drawing.Point(291, 254);
+            this.arm.Name = "arm";
+            this.arm.Size = new System.Drawing.Size(41, 20);
+            this.arm.TabIndex = 0;
+            this.arm.TabStop = false;
+            // 
+            // basket
+            // 
+            this.basket.BackColor = System.Drawing.Color.Goldenrod;
+            this.basket.Location = new System.Drawing.Point(374, 289);
+            this.basket.Name = "basket";
+            this.basket.Size = new System.Drawing.Size(78, 32);
+            this.basket.TabIndex = 0;
+            this.basket.TabStop = false;
+            // 
+            // handle
+            // 
+            this.handle.BackColor = System.Drawing.Color.Goldenrod;
+            this.handle.Location = new System.Drawing.Point(405, 254);
+            this.handle.Name = "handle";
+            this.handle.Size = new System.Drawing.Size(19, 39);
+            this.handle.TabIndex = 0;
+            this.handle.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(643, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(643, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(557, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(557, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(646, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(646, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(682, 399);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.foliage);
+            this.Controls.Add(this.arm);
+            this.Controls.Add(this.body);
+            this.Controls.Add(this.head);
+            this.Controls.Add(this.basket);
+            this.Controls.Add(this.handle);
+            this.Controls.Add(this.trunk);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trunk)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.foliage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.head)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.body)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arm)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basket)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.handle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox trunk;
+        private System.Windows.Forms.PictureBox foliage;
+        private System.Windows.Forms.PictureBox head;
+        private System.Windows.Forms.PictureBox body;
+        private System.Windows.Forms.PictureBox arm;
+        private System.Windows.Forms.PictureBox basket;
+        private System.Windows.Forms.PictureBox handle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- 2024-11-14-Dolgozat/Form1.cs
+++ 2024-11-14-Dolgozat/Form1.cs
@@ -12,9 +12,289 @@ namespace _2024_11_14_Dolgozat
 {
     public partial class Form1 : Form
     {
+        Timer moveCharacter = new Timer();
+        Timer moveApple = new Timer();
+        Timer HandIncrease = new Timer();
+        Timer HandDecrease = new Timer();
+        Timer fallDown = new Timer();
+
+        int points = 10;
+        int armWidth = 25;
+        int basketCap = 20;
+        int punchMultiplier = 10;
+        int hitCounter = 0;
+        bool turnCharacter = false; // ha jobbra fordul akkor true
+        bool isFalling = false;
+        bool goLeft = true;
+        bool goRight = false;
+        List<PictureBox> apples = new List<PictureBox>();
+        int valueA = 10;
+        int valueB = 30;
+
         public Form1()
         {
             InitializeComponent();
+            this.KeyPreview = true;
+            this.BackColor = Color.LightBlue;
+            label1.Text = $"A gyűjtött almák száma: {points}";
+            label2.Text = $"A kosár tehetbírása: {basketCap} alma";
+            label3.Text = "Nagyobb kosár: ";
+            label4.Text = "Gyorsabb szedés: ";
+            button1.Text = $"-{valueA} alma";
+            button2.Text = $"-{valueB} alma";
+            trunk.SendToBack();
+            arm.Width = armWidth;
+            button1.Text = "-10 alma";
+            button2.Text = "-30 alma";
+            label5.Text = $"{hitCounter}";
+            Start();
         }
+
+
+        void Start()
+        {
+            MoveCharacter();
+            BasketIncrease();
+            FastCollect();
+        }
+
+        void MoveCharacter()
+        {
+            moveCharacter.Interval = 50;
+            moveCharacter.Tick += (s, e) =>
+            {
+                if (goLeft)
+                {
+                    MoveCharacterLeft();
+                    if (arm.Bounds.IntersectsWith(trunk.Bounds))
+                    {
+                        moveCharacter.Stop();
+                        goLeft = false;
+                        Punch();
+                        moveCharacter.Start();
+                    }
+                }
+                else if (goRight)
+                {
+                    MoveCharacterRight();
+                    if (arm.Left >= basket.Left)
+                    {
+                        moveCharacter.Stop();
+                        TurnCharacter();
+                        goRight = false;
+                        moveCharacter.Start();
+                    }
+                }
+            };
+            moveCharacter.Start();
+        }
+        void MoveCharacterLeft()
+        {
+            goRight = false;
+            goLeft = true;
+            arm.Left -= 5;
+            head.Left -= 5;
+            body.Left -= 5;
+        }
+
+        void MoveCharacterRight()
+        {
+            goRight = true;
+            goLeft = false;
+            arm.Left += 5;
+            head.Left += 5;
+            body.Left += 5;
+        }
+
+
+        void TurnCharacter()
+        {
+            turnCharacter = !turnCharacter;
+            if (turnCharacter)
+                arm.Left += 20;
+
+            if (!turnCharacter)
+                arm.Left -= 20;
+        }
+
+        void Punch()
+        {
+            hitCounter = 0;
+            goRight = false;
+            if (!isFalling)
+            {
+                HandIncrease.Interval = 200;
+                HandIncrease.Tick += (s, e) =>
+                {
+                    
+                    if (!turnCharacter)
+                    {
+                        arm.Left -= 5;
+                    }
+                    HandIncrease.Stop();
+                    
+                    HandDecrease.Start();
+                };
+                HandIncrease.Start();
+                label5.Text = $"{hitCounter}";
+
+                HandDecrease.Interval = 200;
+                HandDecrease.Tick += (s, e) =>
+                {
+
+                    if (!turnCharacter) {
+                        arm.Left += 5;
+                    }
+                    HandDecrease.Stop();
+
+                    label5.Text = $"{hitCounter}";
+                    if (hitCounter < punchMultiplier )
+                        HandIncrease.Start();
+                   
+                    Hit();
+                    
+                };
+                HandDecrease.Start();
+               
+            }
+          
+        }
+
+        void Hit()
+        {
+            
+            hitCounter++;
+            if (hitCounter == punchMultiplier)
+            {
+                GenerateApple();
+                AppleFall();
+
+              
+            }
+        }
+
+        public void GenerateApple()
+        {
+            apples.Clear();
+            PictureBox alma = new PictureBox();
+            alma.BackColor = System.Drawing.Color.Red;
+            alma.Left = 200;
+            alma.Top = foliage.Bottom;
+            alma.Name = "alma";
+            alma.Size = new System.Drawing.Size(17, 17);
+            alma.BringToFront();
+            apples.Add(alma);
+            this.Controls.Add(alma);
+            isFalling = true;
+        }
+
+
+        void AppleFall()
+        {
+            foreach (PictureBox alma in apples)
+            {
+               
+                alma.BringToFront();
+                fallDown.Interval = 50;
+                fallDown.Tick += (s, e) =>
+                {
+                    alma.Top += 5;
+                    if (arm.Bounds.IntersectsWith(alma.Bounds))
+                    {
+                        fallDown.Stop();
+                        goLeft = false;
+
+                        TurnCharacter();
+
+                        MoveAppleX();
+                        goRight = true;
+                        MoveCharacter();
+                        
+                        if (goRight)
+                            alma.Left = arm.Right - alma.Width / 2;
+                    }
+                };
+                fallDown.Start();
+            }
+        }
+
+        void MoveAppleX()
+        {
+            moveApple.Start();
+
+            if (!goLeft)
+            {
+                foreach (PictureBox alma in apples)
+                    alma.Left += 20;    //fordulás miatti léptetés
+                moveApple.Interval = 50;
+                moveApple.Tick += (s, e) =>
+                {
+                    foreach (PictureBox alma in apples)
+                    {
+                        if (alma.Left < basket.Left)
+                            alma.Left += 10;
+                        if (alma.Left >= basket.Left)
+                        {
+                            alma.Top += 10;
+                            if (alma.Bottom >= basket.Top)
+                            {
+                                moveApple.Stop();
+                                alma.Hide();
+                                
+                                this.Controls.Remove(alma);
+                                isFalling = false;
+                                goRight = false;
+                                goLeft = true;
+                                arm.Left = body.Left -20;
+
+                                if (points < basketCap)
+                                    points++;
+                                label1.Text = $"A gyűjtött almák száma: {points}";
+                                hitCounter = 0;
+                            }
+                        }
+                    }
+                };
+            }
+        }
+
+        void BasketIncrease()
+        {
+            button1.Click += (s, e) =>
+            {
+                if (points >= 10)
+                {
+                    basketCap += 5;
+                    points -= 10;
+                    valueA += 2;
+                    label1.Text = $"A gyűjtött almák száma: {points}";
+                    label2.Text = $"A kosár tehetbírása: {basketCap} alma";
+                    button1.Text = $"-{valueA} alma";
+                }
+                else
+                    MessageBox.Show("Nincs elég almád");
+            };
+        }
+
+
+        void FastCollect()
+        {
+            button2.Click += (s, e) =>
+            {
+                if (points >= valueB && punchMultiplier > 3)
+                {
+
+                    punchMultiplier--;
+                    points -= 30;
+                    valueB += 30;
+                    label1.Text = $"A gyűjtött almák száma: {points}";
+                    button2.Text = $"-{valueB} alma";
+                }
+                else
+                    MessageBox.Show("Nincs elég almád");
+            };
+        }
+
+
     }
 }
